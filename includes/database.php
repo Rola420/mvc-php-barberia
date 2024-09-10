f63950a85e9cd0afdf322c8c94b26e3f1f461776
HugoBG14 <al21510374@tapachula.tecnm.mx>
2024-09-09 22:08:44 -0600

    Modificacion de .env y database.php

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,12 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php'; // Asegúrate de que la ruta sea correcta
+
+// Carga el archivo .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
 $db = mysqli_connect(
     $_ENV['DB_HOST'],
     $_ENV['DB_USER'],
