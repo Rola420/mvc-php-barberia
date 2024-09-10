bc9162ae434c9913f4aba9e541d5c74bffebb6dd
Hugo Borraz <borrazgonzalezhugo@gmail.com>
2024-09-09 22:13:21 -0600

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Asegúrate de que la ruta sea correcta
-
-// Carga el archivo .env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
 
 
 $db = mysqli_connect(
